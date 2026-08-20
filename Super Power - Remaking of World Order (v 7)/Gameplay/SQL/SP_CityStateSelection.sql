7879a6c7b5e35d5e43d8b2386fc25eedbf66b0f3
YANYVDY <136181861+YANYVDU@users.noreply.github.com>
2026-08-20 17:24:51 +0800

    UI: SP_SELECT_CITY_STATES

---
+++ Super Power - Remaking of World Order (v 7)/Gameplay/SQL/SP_CityStateSelection.sql
@@ -0,0 +1,131 @@
+-- Super Power: select city-states at game start (GameOption registration)
+-- Front-end writes selections via PreGame.SetGameOption; DLL reads them in CvGame::init2 -> ApplySelectedMinorCivs
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_ENABLED', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_22', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_23', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_24', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_25', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_26', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_27', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_28', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_29', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_30', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_31', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_32', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_33', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_34', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_35', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_36', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_37', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_38', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_39', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_40', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_41', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_42', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_43', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_44', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_45', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_46', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_47', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_48', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_49', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_50', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_51', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_52', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_53', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_54', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_55', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_56', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_57', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_58', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_59', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_60', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_61', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_62', NULL, NULL, 0, 0, 1, 1);
+
+INSERT INTO GameOptions (Type, Description, Help, "Default", Visible, SupportsSinglePlayer, SupportsMultiplayer)
+VALUES ('GAMEOPTION_SP_CS_63', NULL, NULL, 0, 0, 1, 1);
+
