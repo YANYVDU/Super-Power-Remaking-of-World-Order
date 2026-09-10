a0688ee171530253ebe760a3d1e583335b4fcd3f
YANYVDY <136181861+YANYVDU@users.noreply.github.com>
2026-09-10 21:22:58 +0800

    fix: SpyPoints threshold define missing (0/0 -> 0/100)

--- Super Power - Remaking of World Order (v 7)/Gameplay/SQL/SP_SpyPoints.sql
+++ Super Power - Remaking of World Order (v 7)/Gameplay/SQL/SP_SpyPoints.sql
@@ -6,3 +6,6 @@ ALTER TABLE Buildings ADD COLUMN SpyPoints integer default 0;
 ALTER TABLE Policies ADD COLUMN SpyPoints integer default 0;
 ALTER TABLE Beliefs ADD COLUMN SpyPoints integer default 0;
 ALTER TABLE Traits ADD COLUMN SpyPoints integer default 0;
+
+INSERT INTO Defines(Name, Value) VALUES('SPY_POINTS_THRESHOLD_BASE', 100);
+INSERT INTO Defines(Name, Value) VALUES('SPY_POINTS_THRESHOLD_INCREASE', 50);
